12a05ab9fbc3c7e075e3d0291ffb159fa9ff0131
Anna Wsz <me@ania.ws>
2025-04-22 00:54:04 +0100

    Adapt the template

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { NavCont } from './styles';
 import { Midi } from '../../theming/styles';
-import { Logo } from '../logo';
 import { Switch } from '../switch';
-import { Link, useNavigate } from 'react-router-dom';
 
 const Nav = ({ toggleMode, mode, spread }) => {
-    const navigate = useNavigate();
     return (
         <Midi
             style={{
@@ -21,23 +18,6 @@ const Nav = ({ toggleMode, mode, spread }) => {
                     e.preventDefault();
                 }}
             >
-                <Link
-                    to='/'
-                    aria-current='page'
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
-                            navigate(`/`);
-                        }
-                    }}
-                >
-                    <Logo
-                        style={{
-                            position: 'relative',
-                            display: 'block',
-                            zIndex: 2,
-                        }}
-                    />
-                </Link>
                 <Switch spread={spread} mode={mode} toggleMode={toggleMode} />
             </NavCont>
         </Midi>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import QUERIES, {
+import {
     Main,
     Midi,
     Heading,
-    Full,
-    Image,
-    Paragraph,
 } from '../theming/styles';
-import styled from 'styled-components';
-import { Nav, Foot, Project } from '../components';
+import { Nav, Foot } from '../components';
 import { Spread } from '../components/switch/styles';
-import { Awk1, Vuln, Devops, EA1Image } from '../images';
-
-const Split = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 40px;
-    margin: 200px auto;
-    flex-wrap: wrap;
-
-    > * {
-        width: 100%;
-
-        @media (${QUERIES.large}) {
-            width: calc(50% - 20px);
-        }
-    }
-`;
 
 const Landing = ({ toggleMode, mode, spread, setDisableScroll }) => {
     const spreadClass =
@@ -54,63 +32,11 @@ const Landing = ({ toggleMode, mode, spread, setDisableScroll }) => {
                         color: 'white',
                     }}
                 >
-                    <span style={{ fontWeight: 300 }}>Product Designer</span>{' '}
-                    crafting beautiful
-                    <span style={{ fontStyle: 'italic' }}> digital</span> user
-                    experiences
+                    <span style={{ fontWeight: 300 }}>Hi, I'm Anna</span><br/>
+                    I build things which make sounds <br/>
+                    with <span style={{ fontStyle: 'italic' }}> code</span>
                 </Heading>
             </Midi>
-            <Full>
-                <Image style={{ backgroundImage: `url(${Awk1})` }} />
-            </Full>
-            <Midi>
-                <Split>
-                    <Heading style={{ textAlign: 'left', marginLeft: 0 }}>
-                        What's a UX Engineer?
-                    </Heading>
-                    <Paragraph>
-                        As a UX Engineer, I have the design thinking and visual
-                        expertise of a designer, as well as the technical skills
-                        of a frontend software engineer. <br /> <br />
-                        This allows me to advocate for users and create powerful
-                        visual designs, wireframes, storyboards, and prototypes
-                        while also understanding the technical limitations and
-                        challenges that engineers may face. I'm also able to
-                        code out prototypes and build component libraries with
-                        user needs and accessibility in mind
-                    </Paragraph>
-                </Split>
-            </Midi>
-
-            <Full
-                style={{ display: 'flex', flexDirection: 'column', gap: 160 }}
-            >
-                <Project
-                    name='EA1 Vehicle Service'
-                    image={EA1Image}
-                    url='ea1'
-                    description='Freelance design work done for a client who wanted to create the Uber for car repair and servicing'
-                />
-                <Project
-                    name='Movie Theater Case Study'
-                    url='movie'
-                    description='Reimagining the movie finding and seat selection process for movie theater applications'
-                />
-                <Project
-                    name='IBM, Vulnerability Manager'
-                    image={Vuln}
-                    url='vuln'
-                    description='Enabling IBMers to view the vulnerabilities that exist within their offerings in one easy to understand tool'
-                /> 
-
-                {/* <Project
-                    name='IBM, DevOps Dashboard'
-                    image={Devops}
-                    url='devops'
-                    description='Enabling developers to track and manage their deployments'
-                /> */}
-            </Full>
-
             <Foot />
         </Main>
     );
